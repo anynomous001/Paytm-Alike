e34f3a95d18ba1f5c862add8bd01e9897f0ca499
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-07-06 09:06:11 +0530

    Prisma Addition done

--- apps/userapp/app/page.tsx
+++ apps/userapp/app/page.tsx
@@ -1,7 +1,12 @@
+import { PrismaClient } from "@repo/db/client"
 
+const client = new PrismaClient()
+console.log(client.user)
 
 export default function Home() {
   return (
-    <div className="text-2xl font-semibold" >Hi there</div>
+    <div className="text-2xl font-semibold" >Hi there
+      Prisma added
+    </div>
   );
 }
